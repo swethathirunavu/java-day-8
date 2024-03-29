dce5e9185985000a82af59870ff07cc29f645954
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-29 17:36:04 +0530

    Update and rename README.md to Program1.java

---
+++ Program1.java
@@ -0,0 +1,16 @@
+# java-day-8
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
